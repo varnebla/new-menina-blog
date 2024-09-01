6dd315102ad33328e455cbecc0c0c274e59ad392
Victor Arnedo Blanco <victor@MacBook-Air-de-Victor.local>
2024-09-01 17:26:16 +0200

    feat: add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+  site: 'https://lameninaperdida.art',
+  integrations: [tailwind(), mdx(), sitemap()]
 });
