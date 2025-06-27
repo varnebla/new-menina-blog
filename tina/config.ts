79351b2340432958ec91be502b7db6244de7b73f
varnebla <victor.arnedo.93@gmail.com>
2025-06-27 19:31:54 +0200

    feat: add multiple new posts and images, update content modules and image paths, and modify data store configuration

--- tina/config.ts
+++ tina/config.ts
@@ -21,8 +21,8 @@ export default defineConfig({
   },
   media: {
     tina: {
-      mediaRoot: "src/assets/posts",
-      publicFolder: "",
+      mediaRoot: "/assets/posts",
+      publicFolder: "public",
     },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
